ed086ee0e7cfb8b481e3300f4edcde0b867ebf1b
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-07-07 18:54:14 +0200

    stable build chain called from from node_modules

--- build.js
+++ build.js
@@ -12,15 +12,13 @@ const fse = require('fs-extra');
 const path = require('path');
 const Gaze = require('gaze').Gaze;
 const argv = require('yargs').argv
-const NGC_BINARY='../library-build-chain/node_modules/.bin/ngc';
-const ROLLUP_BINARY='../library-build-chain/node_modules/.bin/rollup';
+const NGC_BINARY='./node_modules/@cloukit/library-build-chain/node_modules/.bin/ngc';
+const ROLLUP_BINARY='./node_modules/@cloukit/library-build-chain/node_modules/.bin/rollup';
 const tsconfigTemplate = require('./build-tsconfig-template.js');
 const packageJsonTemplate = require('./build-package-json-template.js');
 const currentDir = shell.pwd().stdout;
 const relativePath = (_path) => {
   const absolutePath = path.resolve(currentDir, _path);
-  console.log(absolutePath);
-  process.exit();
   return absolutePath;
 }
 
@@ -33,33 +31,10 @@ const buildPackage = (languageTarget, watch) => {
   //
   // CLEANUP COPY SRC CONTENTS TO BUILD
   //
-  if (shell.test('-d', relativePath('../build/src'))) shell.rm('-rf', relativePath('../build/src'));
-  if (shell.test('-f', relativePath('../build/package-lock.json'))) shell.rm('-rf', relativePath('../build/package-lock.json'));
-  if (!shell.test('-d', relativePath('../build/'))) shell.mkdir(relativePath('../build/'));
-  shell.cp('-R', relativePath('../src'), relativePath('../build'));
-  shell.cp('-R', relativePath('../package.json'), relativePath('../build/package_orig.json'));
+  if (!shell.test('-d', relativePath('./build/'))) shell.mkdir(relativePath('./build/'));
 
-  //
-  // CD BUILD DIR
-  //
-  shell.cd(relativePath('../build/'));
-  const manifest = JSON.parse(shell.cat(relativePath('../build/package_orig.json')));
+  const manifest = JSON.parse(shell.cat(relativePath('./package.json')));
 
-  //
-  // GENERATE TEMPORARY LIBRARY package.json TO INSTALL PEER DEPENDENCIES DURING BUILD
-  //
-  if (!watch && languageTarget === 'es5') {
-    let packageJson = packageJsonTemplate.generate(manifest.moduleId, manifest.version, manifest.description, 'devDependencies', Object.assign({}, manifest.peerDependencies, manifest.devDependencies), 'dependencies', manifest.dependencies, 'peerDependencies', {});
-    fs.writeFileSync(relativePath(`../build/package.json`), JSON.stringify(packageJson, null, 2));
-    shell.echo(chalk.blue('>> =============='));
-    shell.echo(chalk.blue('>> YARN INSTALL'));
-    shell.echo(chalk.blue('>> =============='));
-    const npmInstallResult = shell.exec('yarn install');
-    if (npmInstallResult.code !== 0) {
-        shell.echo(chalk.red("YARN ERROR. STOP!"));
-        return;
-    }
-  }
 
   //
   // BUILD OR WATCH
@@ -73,21 +48,24 @@ const buildPackage = (languageTarget, watch) => {
   // WRITE TSCONFIGS
   //
   const tsConfig = tsconfigTemplate.generate(languageTarget, manifest.moduleId);
-  fs.writeFileSync(relativePath(`../build/tsconfig-${languageTarget}.json`), JSON.stringify(tsConfig, null, 2));
+  fs.writeFileSync(relativePath(`./tsconfig-${languageTarget}.json`), JSON.stringify(tsConfig, null, 2));
 
   //
   // BUILD WITH ANGULAR COMPILER
   //
-  const buildResult = shell.exec(`${NGC_BINARY} -p tsconfig-${languageTarget}.json`);
+  const buildResult = shell.exec(`${NGC_BINARY} -p ./tsconfig-${languageTarget}.json`);
+
   if (buildResult.code !== 0) {
       shell.echo(chalk.red("NGC ERROR. STOP!"));
       return;
+  } else {
+    shell.echo(chalk.green(buildResult.stdout));
   }
 
   //
   // BUILD FLAT ONE FILE MODULE WITH ROLLUP
   //
-  const rollupResult = shell.exec(`${ROLLUP_BINARY} _${languageTarget}/src/${manifest.moduleId}.js -o ../dist/${manifest.moduleId}.${languageTarget}.js`);
+  const rollupResult = shell.exec(`${ROLLUP_BINARY} ./build/_${languageTarget}/src/${manifest.moduleId}.js -o ./dist/${manifest.moduleId}.${languageTarget}.js`);
   if (rollupResult.code !== 0) {
       shell.echo(chalk.red("ROLLUP ERROR. STOP!"));
       return;
@@ -102,17 +80,17 @@ const buildPackage = (languageTarget, watch) => {
   // WRITE FINAL LIB package.json
   //
   packageJson = packageJsonTemplate.generate(manifest.moduleId, manifest.version, manifest.description, 'peerDependencies', manifest.peerDependencies, 'dependencies', manifest.dependencies, 'devDependencies', manifest.devDependencies);
-  fs.writeFileSync(relativePath('../dist/package.json'), JSON.stringify(packageJson, null, 2));
+  fs.writeFileSync(relativePath('./dist/package.json'), JSON.stringify(packageJson, null, 2));
 
   //
   // COPY METADATA FILE FOR TREE SHAKING
   //
-  shell.cp(`_es2015/src/${manifest.moduleId}.metadata.json`, `../dist/${manifest.moduleId}.metadata.json`);
+  shell.cp(`./build/_es2015/src/${manifest.moduleId}.metadata.json`, `./dist/${manifest.moduleId}.metadata.json`);
 
   //
   // COPY README
   //
-  shell.cp(`../README.md`, `../dist/`);
+  shell.cp(`./README.md`, `./dist/`);
 
   //
   // FIXME: SINCE WE CANNOT CREATE A TYPE-DEFINITION-BUNDLE FILE (YET) WE NEED TO COPY ALL *.d.ts FILES MANUALLY TO DIST
@@ -120,9 +98,10 @@ const buildPackage = (languageTarget, watch) => {
   shell.echo(chalk.blue('>> =============='));
   shell.echo(chalk.blue(`>> D.TS FILES`));
   shell.echo(chalk.blue('>> =============='));
-  fse.copySync(relativePath('../build/_es2015/src'), relativePath('../dist'), {
+  fse.copySync(relativePath('./build/_es2015/src'), relativePath('./dist'), {
     filter: file => /^.*[.]ts$/.test(file) || shell.test('-d', file) // *.d.ts files and folders!
   });
+  shell.echo(chalk.green('done'));
 
   //
   // COMPODOC
@@ -132,30 +111,28 @@ const buildPackage = (languageTarget, watch) => {
   shell.echo(chalk.blue('>> =============='));
 
   // PATCH CDN URLS
-  if (shell.test('-d', relativePath('../documentation'))) shell.rm('-rf', relativePath('../documentation/'));
-  shell.cd(relativePath('../build'));
+  if (shell.test('-d', relativePath('./documentation'))) shell.rm('-rf', relativePath('./documentation/'));
+  shell.cd(relativePath('./'));
   const cdnUrl = 'https://cloukit.github.io/compodoc-theme/theme/1.0.0-beta.10';
   const templateFiles = [ 'page.hbs', 'partials/component.hbs', 'partials/module.hbs', 'partials/routes.hbs', 'partials/overview.hbs' ];
   for (let i=0; i<templateFiles.length; i++) {
-    shell.exec(`sed -i -e 's@src="[^"]*js/@src="${cdnUrl}/dist/js/@g' ../library-build-chain/node_modules/compodoc/src/templates/${templateFiles[i]}`);
+    shell.exec(`sed -i -e 's@src="[^"]*js/@src="${cdnUrl}/dist/js/@g' ./node_modules/compodoc/src/templates/${templateFiles[i]}`);
   }
-  shell.exec(`sed -i -e 's@href="[^"]*styles/style.css@href="${cdnUrl}/style.css@g' ../library-build-chain/node_modules/compodoc/src/templates/page.hbs`);
-  shell.exec(`sed -i -e 's@href="[^"]*images/favicon.ico@href="${cdnUrl}/images/favicon.ico@g' ../library-build-chain/node_modules/compodoc/src/templates/page.hbs`);
-  shell.exec(`sed -i -e 's@src="[^"]*images/compodoc-vectorise.svg@src="${cdnUrl}/images/compodoc-logo.svg@g' ../library-build-chain/node_modules/compodoc/src/templates/partials/menu.hbs`);
+  shell.exec(`sed -i -e 's@href="[^"]*styles/style.css@href="${cdnUrl}/style.css@g' ./node_modules/compodoc/src/templates/page.hbs`);
+  shell.exec(`sed -i -e 's@href="[^"]*images/favicon.ico@href="${cdnUrl}/images/favicon.ico@g' ./node_modules/compodoc/src/templates/page.hbs`);
+  shell.exec(`sed -i -e 's@src="[^"]*images/compodoc-vectorise.svg@src="${cdnUrl}/images/compodoc-logo.svg@g' ./node_modules/compodoc/src/templates/partials/menu.hbs`);
 
   // EXECUTE COMPODOC
   if (!argv.watch) {
-    shell.cd(relativePath('../build'));
-    const compodocResult = shell.exec(`../library-build-chain/node_modules/compodoc/bin/index-cli.js --tsconfig tsconfig-es5.json --disableCoverage --disablePrivateOrInternalSupport --name "${packageJson.name} v${packageJson.version}" src`);
+    const compodocResult = shell.exec(`./node_modules/compodoc/bin/index-cli.js --tsconfig tsconfig-es5.json --disableCoverage --disablePrivateOrInternalSupport --name "${packageJson.name} v${packageJson.version}" src`);
     if (compodocResult.code !== 0) {
         shell.echo(chalk.red("COMPODOC ERROR. STOP!"));
         return;
     }
-    shell.mv(relativePath('../build/documentation'), relativePath('../'));
-    if (shell.test('-d', relativePath('../documentation/fonts/'))) shell.rm('-rf', relativePath('../documentation/fonts/'));
-    if (shell.test('-d', relativePath('../documentation/images/'))) shell.rm('-rf', relativePath('../documentation/images/'));
-    if (shell.test('-d', relativePath('../documentation/styles/'))) shell.rm('-rf', relativePath('../documentation/styles/'));
-    if (shell.test('-d', relativePath('../documentation/js/'))) shell.rm('-rf', relativePath('../documentation/js/'));
+    if (shell.test('-d', relativePath('./documentation/fonts/'))) shell.rm('-rf', relativePath('./documentation/fonts/'));
+    if (shell.test('-d', relativePath('./documentation/images/'))) shell.rm('-rf', relativePath('./documentation/images/'));
+    if (shell.test('-d', relativePath('./documentation/styles/'))) shell.rm('-rf', relativePath('./documentation/styles/'));
+    if (shell.test('-d', relativePath('./documentation/js/'))) shell.rm('-rf', relativePath('./documentation/js/'));
   }
 
 }
@@ -165,13 +142,13 @@ const buildPackage = (languageTarget, watch) => {
 // INIT
 //
 const initialCleanup = () => {
-  if (shell.test('-d', relativePath('../documentation'))) shell.rm('-rf', relativePath('../documentation/'));
-  if (shell.test('-d', relativePath('../dist'))) shell.rm('-rf', relativePath('../dist/'));
-  shell.mkdir(relativePath('../dist/'));
+  if (shell.test('-d', relativePath('./documentation'))) shell.rm('-rf', relativePath('./documentation/'));
+  if (shell.test('-d', relativePath('./dist'))) shell.rm('-rf', relativePath('./dist/'));
+  shell.mkdir(relativePath('./dist/'));
 };
 
 if (argv.watch) {
-  var gaze = new Gaze('../src/**/*');
+  var gaze = new Gaze('./src/**/*');
   gaze.on('all', (event, filepath) => {
     try {
       //initialCleanup();
