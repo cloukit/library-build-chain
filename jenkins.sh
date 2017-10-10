c4cbe73cbb009121710e38ac3497ec8502da8877
clouless <github@gruenewaldt.net>
2017-10-10 20:38:51 +0200

    v1.3.2 with mocha and chai

--- jenkins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# BUILD TRIGGERED BY: https://github.com/codeclou/jenkins-github-webhook-build-trigger-plugin
-set -e
-git clone https://github.com/cloukit/library-deploy-chain.git library-deploy-chain
-cd library-deploy-chain
-bash jenkins.sh
