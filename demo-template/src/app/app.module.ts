0b61cbe79f5150a45dada74ed77bfa9dd17a337e
clouless <github@gruenewaldt.net>
2017-10-11 22:24:13 +0200

    only one demo import

--- demo-template/src/app/app.module.ts
+++ demo-template/src/app/app.module.ts
@@ -2,19 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DemoComponent } from '../demo/demo.component';
+
+const ngDeclarations = [ AppComponent, DemoComponent ];
+const ngImports = [ BrowserModule ];
+const ngProviders = [ ];
+const ngBootStrap = [ ];
+
 /*___IMPORTS___*/
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    DemoComponent,
-  ],
-  imports: [
-    BrowserModule,
-    /*___NGMODULE_IMPORTS___*/
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: ngDeclarations,
+  imports: ngImports,
+  providers: ngProviders,
+  bootstrap: ngBootStrap
 })
 export class AppModule { }
