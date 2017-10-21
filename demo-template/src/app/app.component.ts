5790b468105b438f9e220f68b1792ef64ce9db5e
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-10-21 16:38:52 +0200

    Update app.component.ts

--- demo-template/src/app/app.component.ts
+++ demo-template/src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="demo">
-      <demo></demo>
-    </div>
-  `
+  template: `<demo></demo>`
 })
 export class AppComponent {
 
